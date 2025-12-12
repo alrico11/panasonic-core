b8071a029c6b25f2bc5e8b4ff5c956c68180f92d
Mochamad Arifin <mochamad.arifin@maxsol.id>
2025-12-12 10:36:12 +0700

    update import path

---
+++ apps/customer-portal/src/customer-portal.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get } from '@nestjs/common';
+import { CustomerPortalService } from './customer-portal.service';
+import { SampleService } from '@lib';
+
+@Controller()
+export class CustomerPortalController {
+  constructor(
+    private readonly customerPortalService: CustomerPortalService,
+    private readonly sampleService: SampleService
+  ) {}
+
+    @Get()
+    getHello(): object {
+        return this.customerPortalService.getWelcome();
+    }
+
+    @Get('sample')
+    getSample(): object {
+        return this.sampleService.getMigrationHistory()
+    }
+
+    @Get('health')
+    getHealth(): object {
+        return { status: 'ok', service: 'customer-portal-api', port: 4001 };
+    }
+
+    @Get('library-data')
+    getLibraryData(): object {
+        return this.customerPortalService.getLibraryData();
+    }
+
+    @Get('library-info')
+    getLibraryInfo(): object {
+        return this.customerPortalService.getLibraryInfo();
+    }
+}
