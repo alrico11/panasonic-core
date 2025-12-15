761187259da458fb511f6b060e5f2df07ac1331a
Abdul Gofar <gofar@trans-cosmos.co.id>
2025-12-15 12:04:12 +0700

    feat(sample): add sample data library to partner-portal

--- apps/partner-portal/src/partner-portal.module.ts
+++ apps/partner-portal/src/partner-portal.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PartnerPortalController } from './partner-portal.controller';
 import { PartnerPortalService } from './partner-portal.service';
+import { LibraryModule } from '@lib';
+import { DatabaseModule } from '@lib';
+import { SampleModule } from '@lib';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -9,6 +12,9 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: ['.env.partner-portal', '.env']
     }),
+    DatabaseModule,
+    LibraryModule,
+    SampleModule
   ],
   controllers: [PartnerPortalController],
   providers: [PartnerPortalService],
